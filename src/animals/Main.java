3d138127538e6e49c9663e2b9f624ee0e66ae264
antilou86 <antilou86@gmail.com>
2019-09-04 17:22:22 -0400

    MVP and Stretch complete.

--- src/animals/Main.java
+++ src/animals/Main.java
@@ -42,17 +42,15 @@ public class main {
         //sortin' em by year
             //this sets up the console for the print.
         System.out.println("All the aminals by most recent year discovered: ");
-        System.out.println(); //makes a new line
             //sorts by year, top to bottom.
         zootopia.sort((animal1, animal2) -> animal2.getYearDiscovered() - animal1.getYearDiscovered());
             //prints a line for each animal in the sorted list
         zooTopia.forEach(animal -> System.out.println("name: "+animal.getName()+" year discovered: "+animal.getYearDiscovered()));
-        System.out.println();
+        System.out.println();//makes a new line
         System.out.println();
 
         //sortin' em by the ABCs
         System.out.println("All the aminals by name, sorted alphabetically: ");
-        System.out.println(); 
         zootopia.sort((animal1, animal2) -> animal1.getName().compareToIgnoreCase(animal2.getName()));
         zooTopia.forEach(animal -> System.out.println("name: "+animal.getName()));
         System.out.println();
@@ -60,12 +58,42 @@ public class main {
 
         //Sortin by movement
         System.out.println("All animals sorted by how they move: ");
-        System.out.println();
         zootopia.sort((animal1, animal2) -> animal1.getMoves().compareToIgnoreCase(animal2.getMoves()));
         zootopia.forEach(animal -> System.out.println(animal.getName()+" will "+animal.moves()+" to move."));
         System.out.println();
         System.out.println();
 
+        //print by breathing with lungs
+        System.out.println("All animals that breathe with lungs: ");
+        printAnimals(zootopia, animal -> animal.breathes().equals("lungs"));
+        System.out.println();        
+        System.out.println();        
+
+        //print by lungs breathers discovered in 1758
+        System.out.println("All animals that breathe with lungs discovered in 1758: ");
+        printAnimals(zootopia, animal -> (animal.breathes().equals("lungs")) && (animal.getYearDiscovered()==1758));
+        System.out.println();        
+        System.out.println();     
         
+        //print by lung breathing-egg layers
+        System.out.println("All animals that breathe with lungs and poop out eggs: ");
+        printAnimals(zootopia, animal -> (animal.breathes().equals("lungs")) && (animal.reproduces().equals("eggs")));
+        System.out.println();        
+        System.out.println();   
+
+        //prints animals discovered in 1758 alphabetically
+        System.out.println("All animals discovered in 1758, aphabetically: ");
+        zootopia.sort((animal1, animal2) -> animal1.getName().compareToIgnoreCase(animal2.getName()));
+        printAnimals(zootopia, animal -> animal.getYearDiscovered()==1758);
+        System.out.println();        
+        System.out.println();
+        
+        //stretch! -sorts alphabetically, then prints out animals that are mammals.-
+        zootopia.sort((animal1, animal2) -> animal1.getName().compareToIgnoreCase(animal2.getName()));
+        for (Animals animal : zootopia) {
+            if (animal instanceof Mammals) {
+                System.out.println("Mammalian detected, name: "+ animal.getName());
+            }
+        }
     }
 }
